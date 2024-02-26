b972937249b3ffe25e4c5525a9e95d366bc223de
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-26 22:56:26 +0100

    added tanstack query

--- VueQuizz/src/App.vue
+++ VueQuizz/src/App.vue
@@ -1,8 +1,18 @@
 <script lang="ts">
+import { QueryClient } from "@tanstack/vue-query";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
+});
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 30_000,
+      refetchOnWindowFocus: true,
+    },
+  },
 });
 </script>
 
