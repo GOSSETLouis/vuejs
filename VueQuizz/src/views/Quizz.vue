71d74a7c4c8dfe2f383de3465625f281ee8c95e7
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-26 17:51:18 +0100

    added main vue and style

---
+++ VueQuizz/src/views/Quizz.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseHeader from "../components/BaseHeader.vue";
+
+export default defineComponent({
+  name: "QuizzDisplay",
+  components: { BaseHeader },
+  setup() {
+    const title = "Bonjour";
+
+    return {
+      title,
+    };
+  },
+});
+</script>
+
+<template>
+  <BaseHeader :title="title" />
+  <div class="flex flex-col gap-4">
+    <h1>Mon Quizz</h1>
+  </div>
+</template>
