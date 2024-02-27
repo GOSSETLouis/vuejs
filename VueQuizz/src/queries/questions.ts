fe9b5aa2fb3e56fce760bd862d7534c925c5760d
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-27 01:04:30 +0100

    fix query questions

--- VueQuizz/src/queries/questions.ts
+++ VueQuizz/src/queries/questions.ts
@@ -1,7 +1,7 @@
 import { QuestionType } from "../types/QuestionType";
 import { baseAxios } from "./axios";
 
-export async function getQuestionsByCategory(category: string): Promise<QuestionType[]> {
+export async function getQuestionsByCategory(category: string) {
   const result = await baseAxios.get<QuestionType[]>(`/questions?categories=${category}`);
   return result.data;
 }
