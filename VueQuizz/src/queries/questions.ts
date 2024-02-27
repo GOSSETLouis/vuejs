46a45e2290faed74f5ca603576508f97e03eec34
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-27 02:09:30 +0100

    added easy mode to query

--- VueQuizz/src/queries/questions.ts
+++ VueQuizz/src/queries/questions.ts
@@ -2,6 +2,8 @@ import { QuestionType } from "../types/QuestionType";
 import { baseAxios } from "./axios";
 
 export async function getQuestionsByCategory(category: string) {
-  const result = await baseAxios.get<QuestionType[]>(`/questions?categories=${category}`);
+  const result = await baseAxios.get<QuestionType[]>(
+    `/questions?categories=${category}&difficulties=easy`
+  );
   return result.data;
 }
