f3f1043c8eaf3113843de902d21aeb69ff68196e
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-27 01:04:41 +0100

    added new route quizz

--- VueQuizz/src/router/router.ts
+++ VueQuizz/src/router/router.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import QuizzVue from "../views/Quizz.vue";
+import HomeVue from "../views/Home.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
+    component: HomeVue,
+  },
+  {
+    path: "/quizz/:category",
+    name: "Quizz",
     component: QuizzVue,
+    props: true,
   },
 ];
 
