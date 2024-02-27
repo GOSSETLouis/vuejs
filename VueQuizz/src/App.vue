536e65ed188a3ae31d82f6e9eb8af46f29f34a1a
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-27 01:04:05 +0100

    fixing queryclient

--- VueQuizz/src/App.vue
+++ VueQuizz/src/App.vue
@@ -1,18 +1,8 @@
 <script lang="ts">
-import { QueryClient } from "@tanstack/vue-query";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
-});
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      staleTime: 30_000,
-      refetchOnWindowFocus: true,
-    },
-  },
 });
 </script>
 
--- VueQuizz/src/main.ts
+++ VueQuizz/src/main.ts
@@ -4,10 +4,21 @@ import App from "./App.vue";
 import { router } from "./router/router";
 import BalmUI from "balm-ui";
 import "balm-ui-css";
+import { QueryClient, VueQueryPlugin } from "@tanstack/vue-query";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App);
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 30_000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
+app.use(VueQueryPlugin);
 app.use(router);
 app.use(BalmUI);
 
