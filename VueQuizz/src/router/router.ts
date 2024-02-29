4d4e8030d053630f3f44f2ecc7c4abc59868bb49
GOSSETLouis <gossetlouis.bdx@gmail.com>
2024-02-29 10:37:43 +0100

    fixed hash in link

--- VueQuizz/src/router/router.ts
+++ VueQuizz/src/router/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import QuizzVue from "../views/Quizz.vue";
 import HomeVue from "../views/Home.vue";
 
@@ -17,6 +17,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
